1544cf3d7c5a31283618c5b43d856cf0d1790240
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-09-09 20:45:44 -0400

    Update apkcombo.py

--- apkd/sources/apkcombo.py
+++ apkd/sources/apkcombo.py
@@ -16,8 +16,9 @@ class Source(BaseSource):
         self.name = 'ApkCombo'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0',
-            'Accept': '*/*',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
+            'Referer': 'https://apkcombo.com/ru/downloader/',
         }
         response = Request.post(
             'https://apkcombo.com/checkin', headers=self.headers)
